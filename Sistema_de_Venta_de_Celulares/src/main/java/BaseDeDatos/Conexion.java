d8779a4f526a8df7f5f6a2f6d1e3e8eeb8dcd8f3
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-02-05 14:24:50 +0100

    :sparkles:feat(BaseDeDatos): Crete connection Mysql

---
+++ Sistema_de_Venta_de_Celulares/src/main/java/BaseDeDatos/Conexion.java
@@ -0,0 +1,18 @@
+package BaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection conectar() {
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/campusan", "campus2023", "campus2023");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return c;
+    }
+}
--- Sistema_de_Venta_de_Celulares/src/main/java/com/mycompany/sistema_de_venta_de_celulares/Main.java
+++ Sistema_de_Venta_de_Celulares/src/main/java/com/mycompany/sistema_de_venta_de_celulares/Main.java
@@ -1,8 +1,10 @@
 package com.mycompany.sistema_de_venta_de_celulares;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
